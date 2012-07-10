f78a5cfacb8a6d52c1e7d2c2fa5410c02d8c36a2
Joey Smith <joeysmith@gmail.com>
2012-07-10 14:12:59 -0600

    Add Austin, TX

---
+++ tx/tr.js
@@ -0,0 +1,10 @@
+function geoip_country_code() { return 'US'; }
+function geoip_country_name() { return 'United States'; }
+function geoip_city() { return 'Austin'; }
+function geoip_region() { return 'TX'; }
+function geoip_region_name() { return 'Texas'; }
+function geoip_latitude() { return '30.2672'; }
+function geoip_longitude() { return '-97.7431'; }
+function geoip_postal_code() { return ''; }
+function geoip_area_code() { return '512'; }
+function geoip_metro_code() { return '635'; }
