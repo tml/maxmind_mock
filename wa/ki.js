132fd5bd1f0010dedf6b370f53f21444941628db
Joey Smith <joeysmith@gmail.com>
2012-07-10 14:17:32 -0600

    Add King County, WA and make both it and Austin, TX selectable

---
+++ wa/ki.js
@@ -0,0 +1,10 @@
+function geoip_country_code() { return 'US'; }
+function geoip_country_name() { return 'United States'; }
+function geoip_city()         { return 'Seattle'; }
+function geoip_region()       { return 'WA'; }
+function geoip_region_name()  { return 'Washington'; }
+function geoip_latitude()     { return '47.6062'; }
+function geoip_longitude()    { return '-122.3321'; }
+function geoip_postal_code()  { return ''; }
+function geoip_area_code()    { return '206'; }
+function geoip_metro_code()   { return '819'; }
