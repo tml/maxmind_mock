bfa1e4f6a5067576ad0aed5d6b644d1bdd6b352f
Joey Smith <joeysmith@gmail.com>
2012-07-10 11:22:07 -0600

    Add a mock from BC, Canada (thanks to the guys at http://bravenet.com/ for the data)

---
+++ cad/bc.js
@@ -0,0 +1,10 @@
+function geoip_country_code() { return 'CA'; }
+function geoip_country_name() { return 'Canada'; }
+function geoip_city()         { return 'Port Alberni'; }
+function geoip_region()       { return 'BC'; }
+function geoip_region_name()  { return 'British Columbia'; }
+function geoip_latitude()     { return '49.2333'; }
+function geoip_longitude()    { return '-124.8000'; }
+function geoip_postal_code()  { return ''; }
+function geoip_area_code()    { return ''; }
+function geoip_metro_code()   { return ''; }
