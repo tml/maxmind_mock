e5cd4493181af4f4fee661651ea56adeb7ee997b
Joey Smith <joeysmith@gmail.com>
2012-07-10 13:25:44 -0600

    Add Orange County, CA

---
+++ ca/oc.js
@@ -0,0 +1,10 @@
+function geoip_country_code() { return 'US'; }
+function geoip_country_name() { return 'United States'; }
+function geoip_city()         { return 'Irvine'; }
+function geoip_region()       { return 'CA'; }
+function geoip_region_name()  { return 'California'; }
+function geoip_latitude()     { return '33.6695'; }
+function geoip_longitude()    { return '-117.8231'; }
+function geoip_postal_code()  { return ''; }
+function geoip_area_code()    { return '714'; }
+function geoip_metro_code()   { return '803'; }
