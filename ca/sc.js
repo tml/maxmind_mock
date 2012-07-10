45f8855c18bd0c3532ffc40db7f6a1ecc582b44c
Joey Smith <joeysmith@gmail.com>
2012-07-10 13:21:47 -0600

    Add San Jose record, make new records selectable

---
+++ ca/sc.js
@@ -0,0 +1,10 @@
+function geoip_country_code() { return 'US'; }
+function geoip_country_name() { return 'United States'; }
+function geoip_city()         { return 'San Jose'; }
+function geoip_region()       { return 'CA'; }
+function geoip_region_name()  { return 'California'; }
+function geoip_latitude()     { return '37.2972'; }
+function geoip_longitude()    { return '-121.8868'; }
+function geoip_postal_code()  { return '95125'; }
+function geoip_area_code()    { return '408'; }
+function geoip_metro_code()   { return '807'; }
