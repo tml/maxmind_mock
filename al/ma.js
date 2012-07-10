1fc01b4a965850699a341bce96d7a2ed3adc3392
Joey Smith <joeysmith@gmail.com>
2012-07-10 16:34:15 -0600

    Added Winfield, Alabama

---
+++ al/ma.js
@@ -0,0 +1,10 @@
+function geoip_country_code() { return 'US'; }
+function geoip_country_name() { return 'United States'; }
+function geoip_city()         { return 'Winfield'; }
+function geoip_region()       { return 'AL'; }
+function geoip_region_name()  { return 'Alabama'; }
+function geoip_latitude()     { return '33.9290'; }
+function geoip_longitude()    { return '-87.8173'; }
+function geoip_postal_code()  { return '35594'; }
+function geoip_area_code()    { return '205'; }
+function geoip_metro_code()   { return '630'; }
